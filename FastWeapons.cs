293e253ba1ee92b19fbd994a16ec6d943f6933fc
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-06-17 19:50:35 -0500

    Update FastWeapons.cs

--- FastWeapons.cs
+++ FastWeapons.cs
@@ -7,14 +7,22 @@ class FastWeapons : GlobalItem
 {
     public override float UseTimeMultiplier(Item item, Player player)
     {
-        if (item.pick > 0 || item.axe > 0 || item.hammer > 0) return 1f;
-        if (item.magic || item.ranged || item.melee || item.ranged || item.thrown || item.summon)
+        if (item.pick > 0 || item.axe > 0 || item.hammer > 0) 
+            return 1f;
+
+        if (item.DamageType == DamageClass.Magic || 
+            item.DamageType == DamageClass.Ranged || 
+            item.DamageType == DamageClass.Melee || 
+            item.DamageType == DamageClass.Ranged || 
+            item.DamageType == DamageClass.Throwing || 
+            item.DamageType == DamageClass.Summon)
         {
             if (item.damage > 0)
             {
                 return FasterDamagesConfig.Instance.WeaponSpeed;
             }
         }
+
         return 1f;
     }
 }
