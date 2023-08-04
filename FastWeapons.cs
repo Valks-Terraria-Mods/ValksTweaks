d266ee3a68fac7cf51b0d47d5e9733b01425267e
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-08-03 23:13:05 -0500

    Many many changes and new features!

--- FastWeapons.cs
+++ FastWeapons.cs
@@ -1,27 +1,108 @@
-﻿using Terraria;
+﻿using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace FasterDamages;
 
-class FastWeapons : GlobalItem
+class GlobalWepItem : GlobalItem
 {
+    public override bool CanConsumeAmmo(Item weapon, Item ammo, Player player)
+    {
+        if (FasterDamagesConfig.Instance.InfiniteAmmo)
+            return false;
+
+        return base.CanConsumeAmmo(weapon, ammo, player);
+    }
+
+    public override bool NeedsAmmo(Item item, Player player)
+    {
+        if (FasterDamagesConfig.Instance.InfiniteAmmo)
+            return false;
+
+        return base.NeedsAmmo(item, player);
+    }
+
+    public override void ModifyShootStats(Item item, Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+    {
+        var config = FasterDamagesConfig.Instance;
+        var projectileType = config.ProjectileType;
+        var maxProjectiles = ProjectileID.Count - 1;
+        var randomProjectiles = new int[]
+        {
+            ProjectileID.BeeArrow,
+            ProjectileID.BloodArrow,
+            ProjectileID.BoneArrow,
+            ProjectileID.ChlorophyteArrow,
+            ProjectileID.CursedArrow,
+            ProjectileID.FireArrow,
+            ProjectileID.FlamingArrow,
+            ProjectileID.FrostArrow,
+            ProjectileID.FrostburnArrow,
+            ProjectileID.HellfireArrow,
+            ProjectileID.HolyArrow,
+            ProjectileID.JestersArrow,
+            ProjectileID.MoonlordArrow,
+            ProjectileID.IchorArrow,
+            ProjectileID.PhantasmArrow,
+            ProjectileID.ShadowFlameArrow,
+            ProjectileID.ShimmerArrow,
+            ProjectileID.UnholyArrow,
+            ProjectileID.VenomArrow,
+            ProjectileID.WoodenArrowFriendly
+        };
+
+        if (config.RandomProjectileType)
+        {
+            type = randomProjectiles[new Random().Next(0, randomProjectiles.Length - 1)];
+        }
+        else if (projectileType != 0)
+            type = type < maxProjectiles ? projectileType : maxProjectiles;
+
+        damage *= FasterDamagesConfig.Instance.ProjectileDamageMultiplier;
+        velocity *= FasterDamagesConfig.Instance.ProjectileVelocityMultiplier;
+    }
+
+    public override bool? CanAutoReuseItem(Item item, Player player)
+    {
+        if (IsTool(item) || !IsWeapon(item))
+            return base.CanAutoReuseItem(item, player);
+
+        return FasterDamagesConfig.Instance.AllWeaponAutoReuse;
+    }
+
+    public override float UseTimeMultiplier(Item item, Player player)
+    {
+        if (IsTool(item) || !IsWeapon(item))
+            return base.UseTimeMultiplier(item, player);
+
+        return FasterDamagesConfig.Instance.UseTimeMultiplier;
+    }
+
     public override float UseSpeedMultiplier(Item item, Player player)
     {
-        if (item.pick > 0 || item.axe > 0 || item.hammer > 0) 
-            return 1f;
+        if (IsTool(item) || !IsWeapon(item))
+            return base.UseAnimationMultiplier(item, player);
 
-        if (item.DamageType == DamageClass.Magic || 
-            item.DamageType == DamageClass.Ranged || 
-            item.DamageType == DamageClass.Melee || 
-            item.DamageType == DamageClass.Throwing || 
-            item.DamageType == DamageClass.Summon)
+        return FasterDamagesConfig.Instance.UseSpeedMultiplier;
+    }
+
+    static bool IsWeapon(Item item) => item.damage > 0;
+
+    static bool IsTool(Item item) => (item.pick > 0 || item.axe > 0 || item.hammer > 0)
+        && !FasterDamagesConfig.Instance.AreToolsEffected;
+}
+
+class GlobalWepProjectile : GlobalProjectile
+{
+    public override bool PreAI(Projectile projectile)
+    {
+        if (projectile.owner == Main.myPlayer)
         {
-            if (item.damage > 0)
-            {
-                return FasterDamagesConfig.Instance.WeaponSpeed;
-            }
+            projectile.tileCollide = !FasterDamagesConfig.Instance.AllProjectilesIgnoreCollision;
         }
 
-        return 1f;
+        return base.PreAI(projectile);
     }
 }
--- FasterDamagesConfig.cs
+++ FasterDamagesConfig.cs
@@ -1,20 +1,48 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using Terraria;
 using Terraria.ModLoader;
 using Terraria.ModLoader.Config;
 
 namespace FasterDamages;
 
-[Label("Faster Weapons Config")]
 public class FasterDamagesConfig : ModConfig
 {
     public override ConfigScope Mode => ConfigScope.ServerSide;
 
-    public static FasterDamagesConfig Instance;
+    public static FasterDamagesConfig Instance => ModContent.GetInstance<FasterDamagesConfig>();
 
-    [Header("Speed")]
-    [Label("Weapon Speed")]
-    [DefaultValue(10f)]
-    [Range(0.5f, 100f)]
-    public float WeaponSpeed;
+    [DefaultValue(1000f)]
+    [Range(0.5f, 1000f)]
+    public float UseSpeedMultiplier;
+
+    [DefaultValue(float.Epsilon)]
+    [Range(float.Epsilon, 1f)]
+    public float UseTimeMultiplier;
+
+    [DefaultValue(true)]
+    public bool AllWeaponAutoReuse;
+
+    [DefaultValue(false)]
+    public bool AreToolsEffected;
+
+    [DefaultValue(false)]
+    public bool InfiniteAmmo;
+
+    [DefaultValue(false)]
+    public bool AllProjectilesIgnoreCollision;
+
+    [DefaultValue(false)]
+    public bool RandomProjectileType;
+
+    [DefaultValue(0)]
+    public int ProjectileType;
+
+    [DefaultValue(1)]
+    [Range(1, 100)]
+    public int ProjectileDamageMultiplier;
+
+    [DefaultValue(1)]
+    [Range(1, 100f)]
+    public float ProjectileVelocityMultiplier;
 }
